a283205ab56fbec33b772610472728c2adbd2622
milich97 <misha199764@gmail.com>
2017-04-02 17:23:20 +0300

    added ability to give parameters

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main; /**
+ * Created by Миша on 27.02.2017.
+ */
+
+import main.FirstWindow;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FirstWindow window1 = new FirstWindow();
+
+    }
+
+
+
+}
+
+
+
+
